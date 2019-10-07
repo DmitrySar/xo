c52b73b2c4b3815c7128e24d608ee5927fdc5bf3
DmitrySar <youshin.d@gmail.com>
2019-10-07 15:57:34 +0300

    write code

--- src/Control.java
+++ src/Control.java
@@ -11,12 +11,13 @@ public class Control {
 	}
 	
 	public void start() {
-        System.out.println("  a b c");
-        System.out.println("1 . . .");
-        System.out.println("2 . . .");
-        System.out.println("3 . . .");
-        while (logic.getNumberOfSteps() < 9){
-            System.out.print("Input coordinates: ");
+		System.out.println("  a b c");
+		System.out.println("1 . . .");
+		System.out.println("2 . . .");
+		System.out.println("3 . . .");
+
+		while (logic.getNumberOfSteps() < 9){
+            System.out.printf("Step of %d. Input coordinates: ", logic.getNumberOfSteps()+1);
 			logic.sendLine(in.nextLine());
 			logic.start();
 		}
